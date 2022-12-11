8b13cf07a590c89905ee76314cf9b149d33bc6cc
shorn1822 <117956756+shorn1822@users.noreply.github.com>
2022-12-11 15:40:29 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -13,7 +13,7 @@ import plotly.graph_objects as go
 
 
 ####creating model 
-s= pd.read_excel("social_media_usage.xlsx")
+s= pd.read_csv("social_media_usage.csv")
 
 def clean_sm(x):
     x= np.where(x==1, 1, 0)
